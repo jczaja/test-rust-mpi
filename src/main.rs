198a7f6cce01022be93b61fe27b252cd2a1a7cad
Jacek Czaja <jacek.czaja@intel.com>
2023-09-28 11:33:38 +0200

    - Added ping pong

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,18 @@
 use mpi::request::WaitGuard;
 use mpi::traits::*;
 
-fn main() {
-    let universe = mpi::initialize().unwrap();
-    let world = universe.world();
-    let size = world.size();
-    let rank = world.rank();
-
+fn basic(comm: &mpi::topology::SystemCommunicator, rank: mpi::Rank, size: mpi::Rank) {
     let next_rank = (rank + 1) % size;
     let previous_rank = (rank - 1 + size) % size;
 
     let msg = vec![rank, 2 * rank, 4 * rank];
     mpi::request::scope(|scope| {
         let _sreq = WaitGuard::from(
-            world
-                .process_at_rank(next_rank)
+            comm.process_at_rank(next_rank)
                 .immediate_send(scope, &msg[..]),
         );
 
-        let (msg, status) = world.any_process().receive_vec();
+        let (msg, status) = comm.any_process().receive_vec();
 
         println!(
             "Process {} got message {:?}.\nStatus is: {:?}",
@@ -29,19 +23,62 @@ fn main() {
         assert_eq!(vec![x, 2 * x, 4 * x], msg);
 
         let root_rank = 0;
-        let root_process = world.process_at_rank(root_rank);
+        let root_process = comm.process_at_rank(root_rank);
 
         let mut a;
-        if world.rank() == root_rank {
+        if comm.rank() == root_rank {
             a = vec![2, 4, 8, 16];
             println!("Root broadcasting value: {:?}.", &a[..]);
         } else {
             a = vec![0; 4];
         }
         root_process.broadcast_into(&mut a[..]);
-        println!("Rank {} received value: {:?}.", world.rank(), &a[..]);
+        println!("Rank {} received value: {:?}.", comm.rank(), &a[..]);
         assert_eq!(&a[..], &[2, 4, 8, 16]);
     });
 }
 
+fn tut2a(comm: &mpi::topology::SystemCommunicator, rank: mpi::Rank, size: mpi::Rank) {
+    if rank == 0 {
+        let data = vec![-1];
+        print!("{rank}: Sending {:?}", data);
+        comm.process_at_rank(1).send(&data);
+    } else if rank == 1 {
+        let (msg, status) = (*comm).any_process().receive_vec::<i32>();
+        println!("{rank}: Recieved {:?}", msg);
+    } else {
+        println!("{rank}: I'm not included in this communication");
+    }
+}
 
+fn tut2b(comm: &mpi::topology::SystemCommunicator, rank: mpi::Rank, size: mpi::Rank) {
+    let mut ping_pong_count = 0;
+    const MAX_SHOTS: i32 = 100;
+    const NUM_PLAYERS: i32 = 2;
+    let partner_rank = (rank + 1) % NUM_PLAYERS; //Where do I send
+
+    while ping_pong_count < MAX_SHOTS {
+        if (rank == ping_pong_count % NUM_PLAYERS) && rank < NUM_PLAYERS {
+            ping_pong_count += 1;
+            comm.process_at_rank(partner_rank).send(&ping_pong_count);
+            println!("{rank}: shooting {ping_pong_count}");
+        } else if rank < NUM_PLAYERS {
+            let (msg, status) = (*comm).process_at_rank(partner_rank).receive::<i32>();
+            println!("{rank}: receiving {msg}");
+            ping_pong_count = msg;
+        } else {
+            println!("{rank}: I'm not playing");
+        }
+    }
+}
+
+fn main() {
+    let universe = mpi::initialize().unwrap();
+    let world = universe.world();
+    let size = world.size();
+    let rank = world.rank();
+
+    //basic(&world,rank,size);
+    //tut2a(&world, rank, size);
+    tut2b(&world, rank, size);
+}
